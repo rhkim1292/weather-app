1d230459a8a0efb76a7a9c9062e0d551ac178780
Randy Kim <rhkim1292@gmail.com>
2022-10-27 00:17:14 -0700

    Implement a function that processes the JSON data
    The JSON data that comes as a resolved response from the api call to
    api.openweathermap.org contains a lot of unused data for this project;
    so a function was implemented to extract only the necessary data from
    the original data to a new object.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import './style.css';
 
-async function getWeather() {
+async function getWeather(location) {
+  if (typeof location !== 'string')
+    throw new Error('A string must be passed into getWeather!');
   try {
     const weather = await fetch(
-      'http://api.openweathermap.org/data/2.5/weather?q=x&APPID=3e233451c61ab3f7ea8c60d2980c48ea'
+      `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&APPID=3e233451c61ab3f7ea8c60d2980c48ea`
     );
     const weatherJSON = await weather.json();
-    console.log(weatherJSON);
+    return weatherJSON;
   } catch (error) {
     console.log(error);
   }
 }
 
-getWeather();
+const processJSON = (promise) => {
+  const processed = {};
+  promise.then((response) => {
+    console.log(response);
+    processed['location'] = response.name;
+    processed['feels_like'] = response.main.feels_like;
+    processed['temp'] = response.main.temp;
+    processed['temp_min'] = response.main.temp_min;
+    processed['temp_max'] = response.main.temp_max;
+    processed['humidity'] = response.main.humidity;
+    processed['weather'] = response.weather[0].main;
+  });
+  return processed;
+};
+
+console.log(processJSON(getWeather('Los Angeles')));
