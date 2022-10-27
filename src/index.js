806e7996a8105140deb91c05f1874cc39ab063ac
Randy Kim <rhkim1292@gmail.com>
2022-10-26 22:41:31 -0700

    Implement an async function
    The async function makes an api call to openweathermap.org to get the
    weather for Los Angeles. A json of the weather data is displayed on the
    console so I can determine how to use the data in the future.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+async function getWeather() {
+  try {
+    const weather = await fetch(
+      'http://api.openweathermap.org/data/2.5/weather?q=x&APPID=3e233451c61ab3f7ea8c60d2980c48ea'
+    );
+    const weatherJSON = await weather.json();
+    console.log(weatherJSON);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getWeather();
